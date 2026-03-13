2814689cefab6026fc2ae275b371e0cfacb00a99
Terron <terronlohanderson@gmail.com>
2026-03-13 21:41:52 +0800

    Changed route path to MyPortfolio

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ import Home from "./pages/Home";
 function Router() {
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
+      <Route path={"/MyPortfolio/"} component={Home} />
       <Route path={"/404"} component={NotFound} />
       <Route component={NotFound} />
     </Switch>
